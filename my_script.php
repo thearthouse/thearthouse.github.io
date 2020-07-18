6dece2a628be61129be994c0a6e4e1ed1a1bfd19
thearthouse <realist.tm@yandex.com>
2020-07-18 20:54:44 +0300

    Update my_script.php

--- my_script.php
+++ my_script.php
@@ -1,4 +1,10 @@
 <?php
-$myfile = file_get_contents("http://abregoslint.000webhostapp.com");
-echo $myfile;
+// the message
+$msg = "First line of text\nSecond line of text";
+
+// use wordwrap() if lines are longer than 70 characters
+$msg = wordwrap($msg,70);
+
+// send email
+mail("realist.tm@yandex.com","My subject",$msg);
 ?>
