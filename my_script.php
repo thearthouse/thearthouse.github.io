08d546acdaf71346902810db18c82ecdf10f181d
thearthouse <realist.tm@yandex.com>
2020-07-18 10:53:24 +0300

    Update my_script.php

--- my_script.php
+++ my_script.php
@@ -1,2 +1,8 @@
 <?php
-echo "i am here";
+$myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
+$txt = "John Doe\n";
+fwrite($myfile, $txt);
+$txt = "Jane Doe\n";
+fwrite($myfile, $txt);
+fclose($myfile);
+?>
