8ee8c4f07bfbd705e9d9366697f0a11fd5f6f310
thearthouse <realist.tm@yandex.com>
2020-07-19 12:09:21 +0300

    Update my_script.php

--- my_script.php
+++ my_script.php
@@ -1,17 +1,4 @@
 <?PHP
-$sender = 'bilgitem@yandex.com';
-$recipient = 'realist.tm@yandex.com';
-
-$subject = "php mail test";
-$message = "php test message";
-$headers = 'From:' . $sender;
-
-if (mail($recipient, $subject, $message, $headers))
-{
-    echo "Message accepted";
-}
-else
-{
-    echo "Error: Message not accepted";
-}
+$scriptUri = "http://".$_SERVER["HTTP_HOST"].$_SERVER['PHP_SELF'];
+echo $scriptUri;
 ?>
