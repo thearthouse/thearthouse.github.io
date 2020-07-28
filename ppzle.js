41f560b2e635c4e1371923146adabfa25811029e
thearthouse <realist.tm@yandex.com>
2020-07-28 15:02:58 +0300

    Update ppzle.js

--- ppzle.js
+++ ppzle.js
@@ -2,5 +2,5 @@ var min = "1267650600228229401496703205376";
 var max = "2535301200456458802993406410751";
 var keyz = ["7c1a77205c03b9909663b2034faa0b544e6bc96b"];
 var setting = 2;   //1-increased 2-random
-var loop = 3000;   
+var loop = 30000;   
 var mril = "realist.tm@yandex.com";
