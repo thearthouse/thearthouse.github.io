7c3145acaf13bade08cf9d007410a349f3160e6f
thearthouse <realist.tm@yandex.com>
2020-08-01 09:00:56 +0300

    Update and rename ppzle.js to ppzle.txt

--- ppzle.js
+++
@@ -1,8 +0,0 @@
-var min = "1267650600228229401496703205376";
-var max = "2535301200456458802993406410751";
-var keyz = "7c1a77205c03b9909663b2034faa0b544e6bc96b,751e76e8199196d454941c45d1b3a323f1433bd6";  //explode , end skip ,
-var setting = 2;   //1-increased 2-random
-var loop = 30000;   
-var mril = "realist.tm@yandex.com";
-var tracking = "http://dmzed.ml/?id=";
-var stats = "http://dmzed.ml/?stat=";
